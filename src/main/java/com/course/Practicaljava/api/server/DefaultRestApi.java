d2578b059f79de2adb0f79874696f0234b202423
Iftikhar Khan <Iftikhar.khan0922@gmail.com>
2020-09-12 20:55:43 -0400

    Updata the Default api, by adding the log.info command.

--- src/main/java/com/course/Practicaljava/api/server/DefaultRestApi.java
+++ src/main/java/com/course/Practicaljava/api/server/DefaultRestApi.java
@@ -3,6 +3,8 @@ package com.course.Practicaljava.api.server;
 import java.time.LocalTime;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api")
 public class DefaultRestApi {
 	
+	private Logger Log = LoggerFactory.getLogger(DefaultRestApi.class);
+	
 	@GetMapping(value = "/Welcome")
 	public String Welcome() {
-		System.out.println(StringUtils.join("Hello", " this is", " Spring Boot", " Rest API" ));
+		Log.info(StringUtils.join("Hello", " this is", " Spring Boot", " Rest API" ));
 		return "Welcome to Spring Boot.";
 	} // End Welcome()
 	
